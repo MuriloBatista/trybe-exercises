da85d9c3a8e8fcfc6814487106bc2d12f66585f1
MuriloBatista <git@github.com:MuriloBatista>
2020-10-27 16:05:08 -0300

    Adiciona exercicio 1 e 2

---
+++ bloco_4/dia_3/exercicio1.js
@@ -0,0 +1,10 @@
+let numero = 5;
+let linha = "*";
+
+for(indexLinha = 1; indexLinha < numero; indexLinha ++){   
+    linha+= "*";
+}
+
+for(indexColuna = 0; indexColuna < numero; indexColuna ++){
+    console.log(linha);
+}
---
+++ bloco_4/dia_3/exercicio2.js
@@ -0,0 +1,9 @@
+let numero = 5;
+let linha = "";
+
+for(indexLinha = 0; indexLinha < numero; indexLinha ++){
+    if (indexLinha < numero){
+        linha += "*";
+    }
+    console.log(linha);
+    }
