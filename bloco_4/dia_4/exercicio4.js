000a6a8be302041935c5427fbd652d5bc63ead64
MuriloBatista <git@github.com:MuriloBatista>
2020-10-28 18:30:05 -0300

    Adiciona arquivos 1,2,3 e 4 do dia 4 do bloco 4

---
+++ bloco_4/dia_4/exercicio4.js
@@ -0,0 +1,16 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
+}
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Chrismas on Bear Mountain, Dell's Four Color Commics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+}
+
+for(objIndex in info){
+    console.log(info[objIndex]);
+}
